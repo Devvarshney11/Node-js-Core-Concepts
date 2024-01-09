0ab1c04a87e6d9253c7115b850a94906a229362e
Dev <devvarshney11@gmail.com>
2024-01-09 17:37:08 +0530

    Adding learning of Event Emmiter

---
+++ EventEmitter/app.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on("foo", () => {
+  console.log("foo event emitted");
+});
+myEmitter.on("foo", () => {
+  console.log("another foo event emitted");
+});
+myEmitter.on("foo", (a, b) => {
+  console.log(`yet another foo event emitted with ${a} and ${b}`);
+});
+
+myEmitter.emit("foo");
+myEmitter.emit("foo", 42, 13);
